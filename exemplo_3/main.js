afa0d5c09edb018c407b15ae8979ee28b14ed42e
Ilora <iloragm1998@gmail.com>
2020-11-17 09:26:54 -0300

    modifiquei e vinculei exemplo_3/index.html e exemplo_3/main.js

---
+++ exemplo_3/main.js
@@ -0,0 +1,10 @@
+/**
+ * JS - Função simples
+ * @author Ilora Gonçalves Monteiro
+ */
+
+ function hello(){
+     var nome;
+     nome = prompt("Qual é o seu nome?")
+     alert("Hello " + nome);
+ }
