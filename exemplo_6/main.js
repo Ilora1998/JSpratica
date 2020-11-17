dbff4a8b71c661e76d0c52e6fffda34f51d28d23
Ilora <iloragm1998@gmail.com>
2020-11-17 12:15:23 -0300

    exemplo_6 modifiquei os arquivos e mostrei as tabuadas

---
+++ exemplo_6/main.js
@@ -0,0 +1,10 @@
+/**
+ * Tabuada
+ * @author Ilora Gonçalves Monteiro
+ */
+
+for (var i = 1; i < 11; i++) {
+    for (var j = 1; j < 11; j++){
+        document.write(i + "*" + j + "=" + i * j + "<br>");
+    }
+}
