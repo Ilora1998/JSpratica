8f725522e2ab0071c7374619b8769c8c71a57ae7
Ilora <iloragm1998@gmail.com>
2020-11-17 13:10:45 -0300

    exemplo_8 finalizado

--- exemplo_8/main.js
+++ exemplo_8/main.js
@@ -3,4 +3,20 @@
  * @author Ilora Gonçalves Monteiro
  */
 
- 
+var intervalo = 3000;
+
+function slide1() {
+    document.getElementById("banner").src = "img/banner1.jpg";
+    setTimeout("slide2()", intervalo);
+}
+
+function slide2() {
+    document.getElementById("banner").src = "img/banner2.jpg";
+    setTimeout("slide3()", intervalo);
+}
+
+function slide3() {
+    document.getElementById("banner").src = "img/banner3.jpg";
+    setTimeout("slide1()", intervalo);
+}
+
