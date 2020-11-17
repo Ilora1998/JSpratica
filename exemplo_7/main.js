3e7c1eec9d0b1680d35e5f0008d63c4d93339947
Ilora <iloragm1998@gmail.com>
2020-11-17 12:52:44 -0300

    exemplo_7 projeto concluído

---
+++ exemplo_7/main.js
@@ -0,0 +1,25 @@
+/**
+ * Controle da Lâmpada
+ * @author Ilora Gonçalves Monteiro
+ */
+
+function on() {
+    document.getElementById("lamp").src = "img/on.jpg";
+}
+
+function off() {
+    document.getElementById("lamp").src = "img/off.jpg";
+}
+
+function blink() {
+    var intervalo = 0;
+    var cont = 0;
+
+    while (cont < 10) {
+        intervalo += 300;
+        setTimeout("document.getElementById('lamp').src='img/on.jpg';", intervalo);
+        intervalo += 300;
+        setTimeout("document.getElementById('lamp').src='img/off.jpg';", intervalo);
+        cont++;
+    }
+}
